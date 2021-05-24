977fc8c8b90d71a0de1bd32df0311502200843e4
Cal Withington <scwithington@gmail.com>
2021-05-24 19:00:01 -0400

    C:/Program Files/Git/pages/Signup/indexjs framework completed

--- client/src/App.js
+++ client/src/App.js
@@ -2,6 +2,7 @@ import logo from './logo.svg';
 import './App.css';
 import API from "./utils/API"
 import { useEffect } from 'react';
+import Signup from './pages/Signup/index.js';
 
 function App() {
 
@@ -17,21 +18,8 @@ function App() {
     }, [])
     
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Signup />
     </div>
   );
 }
