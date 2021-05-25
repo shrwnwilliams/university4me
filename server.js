d90302ec4b08da7d614cb5380d65299bab79651e
DINNERman <shrwnwilliams@gmail.com>
2021-05-25 11:42:30 -0400

    another heroku push

--- server.js
+++ server.js
@@ -2,52 +2,56 @@ const express = require("express");
 const app = express();
 const cors = require("cors");
 const mongoose = require("mongoose");
-const bodyparser = require("body-parser");
+// const bodyparser = require("body-parser");
 const routes = require("./routes");
-const path = require("path")
+const path = require("path");
 // const School = require ("./models/School")
 
 const PORT = process.env.PORT || 3001;
 
-app.use(bodyparser.json())
-app.use(bodyparser.urlencoded({ extended: false}))
-app.use(cors())
+// app.use(bodyparser.json());
+// app.use(bodyparser.urlencoded({ extended: false }));
+app.use(cors());
+app.use(
+  express.urlencoded({
+    extended: true,
+  })
+);
+app.use(express.json());
+app.use(express.static("client/build"));
+// app.get("/api/config", (req, res) => {
+//   res.json({
+//     success: true,
+//   });
+// });
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("client/build"));
+}
 
 app.use(routes);
 
-// School.create({
-//     id: "1",
-//     school: "school",
-//     city: "city",
-//     state: "state",
-//     zipcode: "zip",
-//     url: "url",
-// }).then (schoolModel => console.log(schoolModel))
-// .catch (err => console.log(err))
 
 app.get("*", (req, res) => {
   res.sendFile(path.join(__dirname, "./client/build/index.html"));
-})
+});
 
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/schooldb", 
-{
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/schooldb", {
   useNewUrlParser: true,
   useUnifiedTopology: true,
   useCreateIndex: true,
-  useFindAndModify: false
+  useFindAndModify: false,
 });
 
 var conn = mongoose.connection;
 
-conn.on('connected',()=>{
-    console.log('MongoDB connected')
+conn.on("connected", () => {
+  console.log("MongoDB connected");
 });
 
-conn.on('error',(err)=>{
-    if(err)
-    console.log(err)
+conn.on("error", (err) => {
+  if (err) console.log(err);
 });
 
 app.listen(PORT, () => {
-    console.log(`App running on port ${PORT}!`);
-  });
+  console.log(`App running on port ${PORT}!`);
+});
