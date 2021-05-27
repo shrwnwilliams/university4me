43265df7aac7022f88ebc435b02961365aacc204
DINNERman <shrwnwilliams@gmail.com>
2021-05-27 14:18:34 -0400

    single page college works, still need to put the rest of the info on page

--- client/src/App.js
+++ client/src/App.js
@@ -97,7 +97,7 @@ function App() {
           <Route exact path="/search" component={Search} />
           <Route path="/signup" exact component={Signup} />
           <Route exact path='/profile' component={User} />
-          <Route exact path="/college-page" component={OneSchool} />
+          <Route exact path="/:id" component={OneSchool} />
           {/* <Route exact path='/login' component={Signup} /> */}
           {/* <Route exact path="/login" component={Login} /> */}
         </Wrapper>
--- client/src/components/OneSchool/index.js
+++ client/src/components/OneSchool/index.js
@@ -1,20 +1,51 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { useParams } from "react-router";
+import API from "../../utils/API";
 
 function OneSchool(props) {
+  const [schoolState, setSchoolState] = useState({
+    name: "",
+    city: "",
+    state: "",
+    zip: "",
+    url: "",
+  });
+  const { id } = useParams();
 
-    
+  useEffect(() => {
+    API.getById(id)
+      .then(function (res) {
+        console.log(res.data.results[0]);
+        setSchoolState({
+          name: res.data.results[0]["school.name"],
+          city: res.data.results[0]["school.city"],
+          state: res.data.results[0]["school.state"],
+          zip: res.data.results[0]["school.zip"].split("-")[0],
+          url: res.data.results[0]["school.school_url"],
+        });
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }, []);
+
   return (
     <div className="container">
       <div class="card">
-        <div class="card-header">Featured</div>
+        <div class="card-header">{schoolState.name}</div>
         <div class="card-body">
-          <h5 class="card-title">Special title treatment</h5>
+          <h5 class="card-title"></h5>
           <p class="card-text">
-            With supporting text below as a natural lead-in to additional
-            content.
+            Located in {schoolState.city}, {schoolState.state} {schoolState.zip}
           </p>
-          <a href="#" class="btn btn-primary">
-            Go somewhere
+          <a
+            href={schoolState.url}
+            target="_blank"
+            rel="noreferer noopener"
+            className="btn btn-primary"
+            class="btn btn-primary"
+          >
+            Visit their website
           </a>
         </div>
       </div>
